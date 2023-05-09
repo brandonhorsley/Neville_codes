3dfbd65c63e3497db3f72d243cb6910f449d20c2
Brandon Horsley <brandonhorsley99@hotmail.co.uk>
2023-05-09 16:12:21 +0100

    Rejection sampling done

--- Rej_sampling.py
+++ Rej_sampling.py
@@ -8,3 +8,46 @@ For i = 1 to i = N
    Else:
       Reject x^(i)
 """
+
+#https://jaketae.github.io/study/rejection-sampling/
+
+from scipy.stats import norm
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Target distribution, simple gaussian mixture
+def p(x):
+    return norm.pdf(x, loc=30, scale=10) + norm.pdf(x, loc=80, scale=20)
+
+#Envelope distribution
+def q(x):
+    return norm.pdf(x, loc=50, scale=30)
+
+#Compare plots
+x = np.arange(-50, 151)
+fig, ax = plt.subplots()
+ax.plot(x, p(x), label=r"$p(x)$")
+ax.plot(x, q(x), label=r"$q(x)$")
+plt.legend()
+plt.show()
+
+#Normalisation constant
+M = max(p(x) / q(x))
+
+#Show comparison
+fig, ax = plt.subplots()
+ax.plot(x, p(x), label=r"$p(x)$")
+ax.plot(x, M * q(x), label=r"$k \cdot q(x)$")
+plt.legend()
+plt.show()
+
+def sample(size):
+    xs = np.random.normal(50, 30, size=size)
+    cs = np.random.uniform(0, 1, size=size)
+    mask = p(xs) / (M * q(xs)) > cs
+    return xs[mask] 
+
+samples = sample(10000)
+sns.distplot(samples)
+plt.show()
