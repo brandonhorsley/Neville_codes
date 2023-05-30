94ed560bce3d8a3dd2030377364278a832eacca3
Brandon Horsley <brandonhorsley99@hotmail.co.uk>
2023-05-30 13:52:28 +0100

    Opened code files for generalising my main codes

---
+++ gen_AuxNev.py
@@ -0,0 +1,3 @@
+"""
+This code will be dedicated to generalising Aux_Nev.py so it isn't specifically built around the toy example.
+"""
---
+++ gen_MainCode.py
@@ -0,0 +1,3 @@
+"""
+This code will be dedicated to generalising MainCode.py so it isn't specifically built around the toy example.
+"""
