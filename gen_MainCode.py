eb3697df6d459b1960eb3ed402aa1f5e0e0d3434
Brandon Horsley <brandonhorsley99@hotmail.co.uk>
2023-06-08 11:14:57 +0100

    Further generalisations

--- gen_MainCode.py
+++ gen_MainCode.py
@@ -83,8 +83,8 @@ eta3_true=0.479
 #circuit_ordering=[('BS',0.5),('PS',np.pi),('BS',0.49),('PS',-np.pi),('BS',0.51)]
 #Phase shifter stores a,b as [a,b], i will need to remember this in future components.
 #Additionally i will have to remember that circuit ordering and total ordering is reading from left to right
-#circuit_ordering=[('BS',eta1_true),('PS',[a1_true,b1_true]),('BS',eta2_true),('PS',[a2_true,b2_true]),('BS',eta3_true)]
-circuit_ordering=[('BS',eta2_true),('PS',[a2_true,b2_true]),('BS',eta3_true),('PS',[a1_true,b2_true]),('BS',eta2_true),('BS',eta1_true),('PS',[a1_true,b1_true])]
+circuit_ordering=[('BS',eta1_true),('PS',[a1_true,b1_true]),('BS',eta2_true),('PS',[a2_true,b2_true]),('BS',eta3_true)]
+#circuit_ordering=[('BS',eta2_true),('PS',[a2_true,b2_true]),('BS',eta3_true),('PS',[a1_true,b2_true]),('BS',eta2_true),('BS',eta1_true),('PS',[a1_true,b1_true])]
 circuit = defaultdict(list)
 totalorder=[]
 for k, v in circuit_ordering:
@@ -132,6 +132,10 @@ def constructU_from_p(etas,phis):
 Vmax=5
 N=100 #Top of page 108 ->N=number of experiments
 M=2 #Number of modes
+
+"""
+#commented out since hardcoded
+
 V1=np.random.uniform(low=0, high=Vmax,size=N) #random voltage between 1 and 5 for phase shifter 1
 #V1=V1+rng.normal(scale=0.02, size=N) #Adding gaussian noise, top of page 108 says 2% voltage noise
 V2=np.random.uniform(low=0, high=Vmax,size=N) #random voltage between 1 and 5 for phase shifter 2
@@ -147,8 +151,15 @@ V.append(list(V1))
 V.append(list(V2))
 #print(list(V2))
 #V=V[0]
-V.append(list(V3))
+#V.append(list(V3))
 #V.append(list(V4))
+"""
+V=[]
+for _ in range(len(circuit['PS'])):
+    Velem=V1=np.random.uniform(low=0, high=Vmax,size=N) #random voltage between 1 and 5 for phase shifter 1
+    #V1=V1+rng.normal(scale=0.02, size=N) #Adding gaussian noise, top of page 108 says 2% voltage noise
+    V.append(list(Velem))
+
 expanded_dict= circuit.copy()
 #print(circuit)
 expanded_dict['V'].append(V)
@@ -287,11 +298,46 @@ runtime=(I[-1]/10)+60
 #p_alpha=[0.5,0.5,0.5,0,0,0.5,0.5] #step 2.1
 #p_alpha=[0,0] #step 2.1
 
-#p_alpha_list=[('eta',0.5),('a',0),('b',0.5),('eta',0.5),('a',0),('b',0.5),('eta',0.5)]
-p_alpha_list=[('eta',0.5),('a',0),('b',0.5),('eta',0.5),('a',0),('b',0.5),('eta',0.5),('eta',0.5),('a',0),('b',0.5)]
+"""
+#Commented out since hardcoded
+p_alpha_list=[('eta',0.5),('a',0),('b',0.5),('eta',0.5),('a',0),('b',0.5),('eta',0.5)]
+#p_alpha_list=[('eta',0.5),('a',0),('b',0.5),('eta',0.5),('a',0),('b',0.5),('eta',0.5),('eta',0.5),('a',0),('b',0.5)]
 p_alpha = defaultdict(list)
 for k, v in p_alpha_list:
     p_alpha[k].append(v)
+
+print(p_alpha)
+
+p_alpha['V'].append(V)
+"""
+
+p_alpha_list=[]
+for elem in totalorder:
+    if elem=='BS':
+        newterm=('eta',0.5)
+        p_alpha_list.append(newterm)
+    if elem=='PS':
+        newterm1=('a',0)
+        newterm2=('b',0.5)
+        p_alpha_list.append(newterm1)
+        p_alpha_list.append(newterm2)
+
+p_alpha = defaultdict(list)
+for k, v in p_alpha_list:
+    p_alpha[k].append(v)
+
+"""
+#Automated way of doing it will be to inspect totalorder and count how many 'BS' and 'PS' and use that to inform creation of p_alpha_list
+BSnum=totalorder.count('BS')
+PSnum=totalorder.count('PS')
+#print(p_alpha_list)
+#p_alpha = defaultdict(list)
+#for k, v in p_alpha_list:
+#    p_alpha[k].append(v)
+etalist=[0.5]*BSnum
+alist=[0]*PSnum
+blist=[0.5]*PSnum
+"""
 
 print(p_alpha)
 
