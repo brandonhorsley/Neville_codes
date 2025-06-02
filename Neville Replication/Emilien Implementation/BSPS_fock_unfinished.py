c715d178ec1b2df4a6b81a90881a0ccd05df4d3c
Brandon Horsley <brandonhorsley99@hotmail.co.uk>
2025-06-02 16:20:00 +0100

    Sorted Emilien implementation subfolder

--- Neville Replication/Emilien Implementation/BSPS_fock_unfinished.py
+++ Neville Replication/Emilien Implementation/BSPS_fock_unfinished.py
@@ -1,33 +1,13 @@
 """
-Following repeated failed attempts to fix my own code I am finally at a complete loss for what it 
-could be. So I am changing tactics to instead duplicating Emilien's code and editing from there and 
-hopefully through doing so it will either be made clear or at least the code will be fully functional and 
-I can finally proceed.
+Am using this document to alter BSPS_coherentstate to put in the fock basis rather than using coherent light.
 
 Active notes:
-Changed standard deviations and distributions to Neville thesis ones. Made estimates worse but this 
-should be attributed to lack of burn-in?
 
-Got my MH-within-Gibbs variant and put MH and MH-in-Gibbs as functions. Notable observation is that within-Gibbs means more calls to likelihood 
-which impacts runtime so i took 1000 samples instead to keep runtime good. Estimates for a are still off 
-due to lack of burn-in which impacts other estimates.
+Will only really need to change data draw and loglikelihood, also remember that all we are really doing by getting probability from unitary is extracting the first column (since light is put in the first mode)
 
-Remember that this still has the coherent state approach to it rather than repeated fock states, that can be
-something to keep in mind. Also keep in mind the idea of randomising true values to get a more objective evaluation
-of performance in the end.
+First attempt on changing data draw, will need to comb through and make sure the vectorisation is appropriate.Not only that but also since we are moving into fock basis will need to edit to put more photons in rather than the click of no click from the coherent state example?
 
-Added MIS algorithm. Has issues though since in Neville protocol, MIS is only used to change 'a' and so then if the algorithm is run in isolation 
-then the gaussian kde doesn't work since the other values (eta and b) aren't changing. Error only thrown on eta, could be that eta is all 0.5 while 
-b is 0.7? Either way it is necessary to keep in mind this requirement in using these algorithms but also perhaps put in a simple check like calculation 
-of the cholesky decomposition and if an error is raised then automatically rerun the code?
-
-Added MIS algorithm that works on all terms, so MIS stuff all sorted.
-
-Added MIS-within-Gibbs (both alpha model - meaning just a's - and full model so all terms). Both appear to work as expected.
-
-Added pi kick search, will only make an alpha model version, won't bother with the full version. Still standby that i think pi kick is a bit crap since it only works if your estimate for a is pi out, probably better for multiple phase shifters so obvs won't really work well in a single MZI unless your starting guess is far out, e.g. start at pi when the truly value is around zero, even then it should minimise after at most a few iterations. Tested so it works properly.
-
-Changed phi to a+bV**2
+Looks like this code may be abandoned since with the project looking more in a boson sampling direction, the bayesian stuff isn't necessary so we may as well start from scratch.
 """
 
 import numpy as np
@@ -43,8 +23,10 @@ eta = 0.521  # arbitrary value for testing
 
 mu = 0.5  # coherent state intensity
 n = 10000  # number of data sample tuples (outcome, voltage)
-N = 50000  # size of MCMC
+#N = 50000  # size of MCMC
 #N=1000
+I=[2,500,50,50,500,100,100,100000] #Determines iteration number for each algorithm call
+#I=[2,500,50,50,500,100,100,1000] #Smaller MCMC chain for troubleshooting
 
 sigma_hyper = 0.5  # variance for proposal distribution
 eta_sigma=0.005
@@ -59,8 +41,9 @@ def data_draw(x, n):
     v = np.random.uniform(size=(n, ))  # draw voltage at random
     v = v[:, np.newaxis].repeat(x.shape[0], axis=1)  # duplicate the value of voltage for each value of a, b, eta
     #phi = x[np.newaxis, :, 0] * v ** 2 + x[np.newaxis, :, 1]  # a * V^2 + b, shape (n, m)
-    phi = x[np.newaxis, :, 0] + x[np.newaxis, :, 1] * v ** 2 # a + b*V^2, shape (n, m)
+    phi = x[np.newaxis, :, 0] + x[np.newaxis, :, 1] * v ** 2  # a + b*V^2 + b, shape (n, m)
     exp_phi = np.exp(1j * phi)
+    """
     # we shouldn't have negative value under sqrt here because data is generated from realistic value
     sqrt_eta = np.sqrt(x[np.newaxis, :, 2])
     sqrt_1eta = np.sqrt(1-x[np.newaxis, :, 2])
@@ -81,6 +64,41 @@ def data_draw(x, n):
     y = np.argmax(u[np.newaxis, :, :] < z, axis=0)  # selected outcome value, shape (n, m)
     #print(y)
     Y = np.concatenate([y[:, :, np.newaxis], v[:, :, np.newaxis]], axis=-1)  # shape (n,m,2)
+    """
+    
+    #Need to inspect dimensions for the below block since i doubt i got it right first time
+    sqrt_eta = np.sqrt(x[np.newaxis, :, 2])
+    sqrt_1eta = np.sqrt(1-x[np.newaxis, :, 2])
+    #U_BS=np.array([[sqrt_eta[0][0],sqrt_1eta[0][0]],[sqrt_1eta[0][0],-sqrt_eta[0][0]]])
+    #Remember that exp_phi is an array not a scalar value
+    #U_PS=np.array([[exp_phi,0],[0,1]])
+    #print(exp_phi[0][0])
+    #U_PS=np.array([[exp_phi[0][0],0],[0,1]])
+    #print(U_PS)
+    #print(U_BS)
+    #U=U_BS@U_PS
+    #P1=abs(U[0][0])**2
+    #P2=abs(U[1][0])**2
+    #print(P1) 
+    #print(P2)
+
+    #If we are putting PS before BS:
+    P1=np.abs(sqrt_eta*exp_phi)**2 #need to sort out dims since this is absoluting and squaring whole array
+    P2=np.abs(sqrt_1eta*exp_phi)**2
+    
+    #print(P1+P2) 
+    #print(exp_phi)
+    #print(sqrt_eta)
+    #print()
+    #print(sqrt_eta*exp_phi)
+    #print(P1) #looks like the same value but is actually slightly different each time, maybe exp_phi isn't wide enough range?
+    #print(P2)
+    #print()
+    #P = np.cumsum(P1, axis=0)
+    y=np.concatenate([P1,P2],axis=0)
+    #print(y)
+    Y = np.concatenate([y[:, :, np.newaxis], v[:, :, np.newaxis]], axis=-1)  # shape (n,m,2)
+    
     return Y
 
 
@@ -89,7 +107,7 @@ def data_draw(x, n):
 # x: scalar parameter we want to estimate, shape (m, 3)
 def loglikelihood(y, x):
     #phi = x[np.newaxis, :, 0] * y[:, np.newaxis, 1]**2 + x[np.newaxis, :, 1]  # a * V^2 + b, shape (n, m)
-    phi = x[np.newaxis, :, 0] + x[np.newaxis, :, 1] * y[:, np.newaxis, 1]**2   # a + b*V^2, shape (n, m)
+    phi = x[np.newaxis, :, 0] + x[np.newaxis, :, 1] * y[:, np.newaxis, 1] ** 2  # a + b*V^2 + b, shape (n, m)
     exp_phi = np.exp(1j*phi)
     # here we can possibly test unrealistic values leading to negative sqrt, so numerical result is unreliable
     # however for such values, the prior should be such that these points will always be rejected in the acceptance test
@@ -232,11 +250,11 @@ def pi_draw(x1):
 
 y = data_draw(x=np.array([[a, b, eta]]), n=n).reshape((-1, 2))  # draw data from exact model
 y = y.reshape((-1, 2))  # reshape to remove the axis for x since we have m=1; not sure if it's relevant to have any m>1
-x = np.array([[0.1, 0.7, 0.5]])  # initial point
+x = np.array([[0.5, 0.1, 0.5]])  # initial point
 #x=np.array([[np.pi,0.7,0.5]]) #initial point array to test pi kick alg
 
 #Alg4
-def MCMC_MHinGibbs(x):
+def MCMC_MHinGibbs(x,N, ReturnLast=True):
     #MH-within-Gibbs alg
     L = [x]  # output chain
     for _ in range(N):
@@ -272,13 +290,89 @@ def MCMC_MHinGibbs(x):
                 if u <= np.exp(A):  # TODO array version, likely need cleanup
                     x = x2
         L.append(x)
+    #Hashing out warmup since need to only take last result for most of these algorithms
+    #warmup = N // 10
+    #L = L[warmup:]  # dump first 10% of the chain
+    if ReturnLast:
+        state=L[-1]
+        return state
+    else:
+        chain = np.array(L)
+        chain = chain.reshape((-1, 3))
+        chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
+        return chain
 
-    warmup = N // 10
-    L = L[warmup:]  # dump first 10% of the chain
-    chain = np.array(L)
-    chain = chain.reshape((-1, 3))
-    chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
-    return chain
+#Alg4_alpha
+def MCMC_MHinGibbs_a(x,N, ReturnLast=True):
+    #MH-within-Gibbs alg
+    L = [x]  # output chain
+    for _ in range(N):
+        for i in range(len(x[0])):
+            if i in [0]:
+                u = np.random.uniform()  # uniform draw to test acceptance
+                x2 = a_proposal_draw(x1=x)  # draw a single candidate
+
+                LL2 = logposterior(y=y, x=x2) + logproposal_distribution(x2=x, x1=x2)
+                LL1 = logposterior(y=y, x=x) + logproposal_distribution(x2=x2, x1=x)
+                LLdifference = LL2 - LL1
+                A = np.where(LLdifference >= 0, 0, LLdifference)
+                if u <= np.exp(A):  # TODO array version, likely need cleanup
+                    x = x2
+        L.append(x)
+    #Hashing out warmup since need to only take last result for most of these algorithms
+    #warmup = N // 10
+    #L = L[warmup:]  # dump first 10% of the chain
+    if ReturnLast:
+        state=L[-1]
+        return state
+    else:
+        chain = np.array(L)
+        chain = chain.reshape((-1, 3))
+        chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
+        return chain
+
+#chain=MCMC_MHinGibbs_a(x)
+
+#Alg4_beta
+def MCMC_MHinGibbs_b(x,N, ReturnLast=True):
+    #MH-within-Gibbs alg
+    L = [x]  # output chain
+    for _ in range(N):
+        for i in range(len(x[0])):
+            if i in [0]:
+                u = np.random.uniform()  # uniform draw to test acceptance
+                x2 = a_proposal_draw(x1=x)  # draw a single candidate
+
+                LL2 = logposterior(y=y, x=x2) + logproposal_distribution(x2=x, x1=x2)
+                LL1 = logposterior(y=y, x=x) + logproposal_distribution(x2=x2, x1=x)
+                LLdifference = LL2 - LL1
+                A = np.where(LLdifference >= 0, 0, LLdifference)
+                if u <= np.exp(A):  # TODO array version, likely need cleanup
+                    x = x2
+            if i in [1]:
+                u = np.random.uniform()  # uniform draw to test acceptance
+                x2 = b_proposal_draw(x1=x)  # draw a single candidate
+
+                LL2 = logposterior(y=y, x=x2) + logproposal_distribution(x2=x, x1=x2)
+                LL1 = logposterior(y=y, x=x) + logproposal_distribution(x2=x2, x1=x)
+                LLdifference = LL2 - LL1
+                A = np.where(LLdifference >= 0, 0, LLdifference)
+                if u <= np.exp(A):  # TODO array version, likely need cleanup
+                    x = x2
+        L.append(x)
+    #Hashing out warmup since need to only take last result for most of these algorithms
+    #warmup = N // 10
+    #L = L[warmup:]  # dump first 10% of the chain
+    if ReturnLast:
+        state=L[-1]
+        return state
+    else:
+        chain = np.array(L)
+        chain = chain.reshape((-1, 3))
+        chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
+        return chain
+    
+#chain=MCMC_MHinGibbs_b(x)
 
 #chain=MCMC_MHinGibbs(x)
 
@@ -286,7 +380,7 @@ def MCMC_MHinGibbs(x):
 """
 Warning this function will throw an error if only this algorithm is run since i have constructed it like the NEville thesis version where it is only proposing new 'a' values so b and eta don't change thus leading to issues with the gaussian kde for those chains.
 """
-def MCMC_MIS_a(x):
+def MCMC_MIS_a(x,N, ReturnLast=True):
     #MH algorithm
     L = [x]  # output chain
     for _ in range(N):
@@ -300,16 +394,21 @@ def MCMC_MIS_a(x):
         if u <= np.exp(A):  # TODO array version, likely need cleanup
             x = x2
         L.append(x)
-    warmup = N // 10
-    L = L[warmup:]  # dump first 10% of the chain
-    chain = np.array(L)
-    chain = chain.reshape((-1, 3))
-    chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
-    return chain
+    #Hashing out warmup since need to only take last result for most of these algorithms
+    #warmup = N // 10
+    #L = L[warmup:]  # dump first 10% of the chain
+    if ReturnLast:
+        state=L[-1]
+        return state
+    else:
+        chain = np.array(L)
+        chain = chain.reshape((-1, 3))
+        chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
+        return chain
 
 #chain=MCMC_MIS_a(x)
 
-def MCMC_MIS(x):
+def MCMC_MIS(x,N):
     #MH algorithm
     L = [x]  # output chain
     for _ in range(N):
@@ -336,7 +435,7 @@ def MCMC_MIS(x):
 """
 Warning this function will throw an error if only this algorithm is run since i have constructed it like the NEville thesis version where it is only proposing new 'a' values so b and eta don't change thus leading to issues with the gaussian kde for those chains.
 """
-def MCMC_MISinGibbs_a(x):
+def MCMC_MISinGibbs_a(x,N, ReturnLast=True):
     #MIS-within-Gibbs alg
     L = [x]  # output chain
     for _ in range(N):
@@ -352,17 +451,21 @@ def MCMC_MISinGibbs_a(x):
                 if u <= np.exp(A):  # TODO array version, likely need cleanup
                     x = x2
         L.append(x)
-
-    warmup = N // 10
-    L = L[warmup:]  # dump first 10% of the chain
-    chain = np.array(L)
-    chain = chain.reshape((-1, 3))
-    chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
-    return chain
+    #Hashing out warmup since need to only take last result for most of these algorithms
+    #warmup = N // 10
+    #L = L[warmup:]  # dump first 10% of the chain
+    if ReturnLast:
+        state=L[-1]
+        return state
+    else:
+        chain = np.array(L)
+        chain = chain.reshape((-1, 3))
+        chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
+        return chain
 
 #chain=MCMC_MISinGibbs_a(x)
 
-def MCMC_MISinGibbs(x):
+def MCMC_MISinGibbs(x,N):
     #MIS-within-Gibbs alg
     L = [x]  # output chain
     for _ in range(N):
@@ -412,7 +515,7 @@ def MCMC_MISinGibbs(x):
 """
 Warning this function will throw an error if only this algorithm is run since i have constructed it like the NEville thesis version where it is only proposing new 'a' values so b and eta don't change thus leading to issues with the gaussian kde for those chains.
 """
-def MCMC_pi_kick(x):
+def MCMC_pi_kick(x,N, ReturnLast=True):
     L = [x]  # output chain
     for _ in range(N):
         x2=pi_draw(x1=x) # draw candidate
@@ -424,16 +527,21 @@ def MCMC_pi_kick(x):
         if LLdifference>0:
             x=x2
         L.append(x)
-    warmup = N // 10
-    L = L[warmup:]  # dump first 10% of the chain
-    chain = np.array(L)
-    chain = chain.reshape((-1, 3))
-    chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
-    return chain
+    #Hashing out warmup since need to only take last result for most of these algorithms
+    #warmup = N // 10
+    #L = L[warmup:]  # dump first 10% of the chain
+    if ReturnLast:
+        state=L[-1]
+        return state
+    else:
+        chain = np.array(L)
+        chain = chain.reshape((-1, 3))
+        chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
+        return chain
 
 #chain=MCMC_pi_kick(x)
 
-def MCMC_MH(x):
+def MCMC_MH(x,N):
     #MH algorithm
     L = [x]  # output chain
     for _ in range(N):
@@ -454,7 +562,57 @@ def MCMC_MH(x):
     chain = chain.transpose()  # not very meaningful to transpose, just simpler for kde call later
     return chain
 
-chain=MCMC_MH(x)
+#chain=MCMC_MH(x)
+
+p_alpha=x
+
+for i in range(I[0]): #step 2.2
+    #step 2.2i
+    p_alpha=MCMC_MIS_a(p_alpha,I[1],ReturnLast=True)
+    print(p_alpha)
+    print("###step 2.2i done###")
+    #step 2.2ii
+    p_alpha=MCMC_MISinGibbs_a(p_alpha,I[2],ReturnLast=True)
+    print(p_alpha)
+    print("###step 2.2ii done###")
+    #step 2.2iii
+    #p_alpha=Alg4(p_alpha, I[3]) #p_alpha is first p_alpha
+    p_alpha=MCMC_MHinGibbs_a(p_alpha, I[3],ReturnLast=True) #p_alpha is second p_alpha
+    print(p_alpha)
+    print("###step 2.2iii done###")
+    #step 2.2iv (and 2.2v)
+    p_alpha=MCMC_pi_kick(p_alpha,I[4],ReturnLast=True)
+    print(p_alpha)
+    print("###step 2.2iv done###")
+
+#p_beta=[0.5,0.5,0.5,p_alpha[3],p_alpha[4],b_est,b_est] #step 2.3
+#p_beta=[0.5,0.5,0.5,p_alpha[3],p_alpha[4],0.7,0.7]
+p_beta=p_alpha
+
+print("p_beta initial is: {}".format(p_beta))
+#step 2.4
+p_beta=MCMC_MHinGibbs_b(p_beta, I[5],ReturnLast=True)
+print(p_beta)
+print("###step 2.4 done###")
+
+#p_zero=[0.5,0.5,0.5,p_beta[3],p_beta[4],p_beta[5],p_beta[6]] #step 2.5
+p_zero=p_beta
+print("p_zero is: {}".format(p_zero))
+
+#step 2.6
+p_zero=MCMC_MHinGibbs(p_zero,I[6], ReturnLast=True)
+print(p_zero)
+print("###step 2.6 done###")
+
+#p_conv=list(p_zero) #step 2.7
+p_conv=p_zero
+print("p_conv is: {}".format(p_conv))
+
+
+###Main Markov Chain Generation###
+
+#Step 3
+chain=MCMC_MHinGibbs(p_conv,I[7], ReturnLast=False)
 
 # Data visualisation
 # I no longer plot the exact posterior because it's a pain when using loglikelihood
@@ -478,6 +636,16 @@ plt.xlim([0, 1])
 # plt.show()
 plt.savefig("a.png")
 
+plt.figure()
+plt.plot(chain[0], color="blue")
+plt.axhline(y=a, label="Exact a", color="red")
+plt.legend()
+#plt.ylim([0, 1])
+#plt.xlim([0, np.round(1.2*np.max([estimated_pdf_a]))])
+# plt.show()
+plt.savefig("a_state.png")
+
+
 # marginal in b
 kernel_b = scipy.stats.gaussian_kde(chain[1])
 b_range = np.linspace(-1, 1, 1000)
@@ -497,6 +665,14 @@ plt.xlim([-1, 1])
 # plt.show()
 plt.savefig("b.png")
 
+plt.figure()
+plt.plot(chain[1], color="blue")
+plt.axhline(y=b, label="Exact b", color="red")
+plt.legend()
+#plt.ylim([-1, 1])
+#plt.xlim([0, np.round(1.2*np.max([estimated_pdf_b]))])
+# plt.show()
+plt.savefig("b_state.png")
 
 # marginal in eta
 kernel_eta = scipy.stats.gaussian_kde(chain[2])
@@ -516,3 +692,12 @@ plt.xlim([0, 1])
 #plt.ylim([0, np.round(1.2*np.max([estimated_pdf_eta]))])
 # plt.show()
 plt.savefig("eta.png")
+
+plt.figure()
+plt.plot(chain[2], color="blue")
+plt.axhline(y=eta, label="Exact eta", color="red")
+plt.legend()
+#plt.ylim([0, 1])
+#plt.xlim([0, np.round(1.2*np.max([estimated_pdf_eta]))])
+# plt.show()
+plt.savefig("eta_state.png")
